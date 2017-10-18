b9c2c81c3105bda8c339e23d09b35cb086d665b8
apellizz <alberto.pellizzon@hotmail.it>
2017-10-18 09:40:41 +0200

    Handle same pair / wrong pair

--- src/screens/CardsScreen/index.js
+++ src/screens/CardsScreen/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { View } from 'react-native';
 import CardList from '../../components/CardList';
-import { Text, View } from 'react-native';
 import styles from './styles';
 
 class CardsScreen extends React.PureComponent {
-
   constructor(props) {
     super(props);
 
@@ -18,20 +17,43 @@ class CardsScreen extends React.PureComponent {
     this.state = {
       cards,
       rows,
+      previousCard: null,
     };
   }
 
   show = card => ({ ...card, flipped: true })
 
+  hide = card => ({ ...card, flipped: false })
+
   reveal = index =>
-    this.state.cards.map(card => card.index === index ? this.show(card) : card)
+    this.state.cards.map(card => (card.index === index ? this.show(card) : card))
+
+  rollback = (indexA, indexB) => {
+    this.setState({
+      cards: this.state.cards.map((card) => {
+        if (card.index === indexA || card.index === indexB) {
+          return this.hide(card);
+        }
+        return card;
+      }),
+    });
+  }
+
 
   flip = (index, label) => {
-    this.setState({ cards: this.reveal(index) })
+    const previousCard = this.state.previousCard;
+    if (!previousCard) {
+      this.setState({ cards: this.reveal(index), previousCard: { index, label } });
+    } else {
+      if (label !== previousCard.label) {
+        setTimeout(() => this.rollback(previousCard.index, index), 1500);
+      }
+      this.setState({ cards: this.reveal(index), previousCard: null });
+    }
   }
 
   render() {
-    return(
+    return (
       <View style={styles.container}>
         <CardList {...this.state} flip={this.flip} />
       </View>
